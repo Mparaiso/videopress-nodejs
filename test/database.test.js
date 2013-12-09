7be18a25083eba4e9a99bc9685f6a9262262aef2
mparaiso <aikah@free.fr>
2013-12-09 17:06:17 +0100

    update Mon Dec  9 16:06:16 GMT 2013

--- test/database.test.js
+++ test/database.test.js
@@ -1,12 +1,23 @@
 /*global describe,it,beforeEach */
 "use strict";
 var db=require('../lib').database;
+var duration = require('../lib').duration;
 var assert=require('assert');
 
 describe("DATABASE",function(){
 	describe("Video model",function(){
 		beforeEach(function(){
 			this.video= new db.model('Video');
+			this.video.set({
+				title:'a video',
+				description:'description',
+				url:'http://something.web.com',
+				thumbail:'http://web.com/image.png',
+				originalId:'3DFDDFDDFDF',
+				provider:'youtube',
+				duration:duration.parse('PT15M30S'),
+				publishedAt:new Date('2013-12-11')
+			});
 		});
 	});
 });
