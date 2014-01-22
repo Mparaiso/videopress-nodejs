7c270df4d82e5b517568e6d240179ccd1c88ebd9
mparaiso <aikah@free.fr>
2014-01-22 17:06:51 +0100

    autocommit Wed Jan 22 16:06:52 GMT 2014

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ express = require('express');
 http = require('http');
 app = express();
 app.set('port', process.env.VCAP_APP_PORT || 3000);
+app.use(express.favicon());
 app.get('/', function(req, res) {
 	res.end('OK!');
 });
