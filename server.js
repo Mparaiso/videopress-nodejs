05d45c103df33f13d496bc15a3bcde4318bee7a8
mparaiso <aikah@free.fr>
2014-01-22 16:29:41 +0100

    simplifications pour tests sur af

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 "use strict";
+var app, express, http;
 
-var app = require('./index');
-var http = require('http');
-
-app.set('port',process.env.VCAP_APP_PORT||3000);
+express = require('express');
+http = require('http');
+app = express();
+app.set('port', process.env.VCAP_APP_PORT || 3000);
+app.get('/', function(request, response) {
+	response.end('OK!');
+});
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
